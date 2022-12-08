d283796168ba46cfcfc95cae94f6fb4badfb5506
Muser0030 <adiehelvy@gmail.com>
2022-12-08 13:07:29 -0800

    task 4 c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,10 @@
 #include <stdio.h>
 /**
- * main - Entry Point
- *
- * Return: Always 0 (Success)
+ * main - Entry
+ * Return: Always 0
  */
 int main(void)
 {
-	puts("\"Programming is like building the multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
-
 }
