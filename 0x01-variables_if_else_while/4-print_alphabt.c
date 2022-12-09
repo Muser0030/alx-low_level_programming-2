9a3e69044412724e502687ac22f544311820c15b
Muser0030 <117852918+Muser0030@users.noreply.github.com>
2022-12-10 02:37:38 +0300

    Update 4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,18 +1,24 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
- * main - prints the alphabet in lowercase
- * followed by a new line, except q and e
- * Return: Always 0
+ * main - returns alphabets both except e and q
+ *
+ * Return: Always 0 (Success)
  */
-int main(void)
-{
-	char la;
 
-	for (la = 'a'; la <= la = 'z'; la++);
+int main(void)
+
+{
+	char ch = 'a';
+
+	while (ch <= 'z')
 	{
-		if (la != 'e' && la != 'q')
-			putchar(la);
+		if ((ch != 'e') && (ch != 'q'))
+		{
+			putchar(ch);
+		}
+		ch++;
 	}
 	putchar('\n');
 	return (0);
